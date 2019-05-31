23ecc5361310252b4b39e4a63c932652d52fa2e8
Ron Gilmour <rgilmour70@gmail.com>
2019-05-31 17:53:14 -0400

    initial commit

---
+++ src/ably.js
@@ -0,0 +1,4 @@
+import { Realtime } from 'ably/browser/static/ably-commonjs.js';
+
+// alert("You need to add your API key"); /* REMOVE THIS */
+window.Ably = new Realtime('tm4Dxg.q9QnHw:tTO_oaYinBuHi3-d'); // ADD YOUR API KEY HERE
---
+++ src/components/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import CommentBox from './CommentBox';
+import Comments from './Comments';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleAddComment = this.handleAddComment.bind(this);
+    this.state = {
+      comments: []
+    }
+  }
+
+  componentDidMount() {
+    /*global Ably*/
+    const channel = Ably.channels.get('comments');
+
+    channel.attach();
+    channel.once('attached', () => {
+      channel.history((err, page) => {
+        // create a new array with comments only in an reverseved order (i.e old to new)
+        const comments = Array.from(page.items.reverse(), item => item.data)
+
+        this.setState({ comments });
+
+        // from Tom at Ably
+        channel.subscribe((msg) => {
+          const commentObject = msg['data'];
+          this.handleAddComment(commentObject);
+        })
+
+      });
+    });
+  }
+
+  handleAddComment(comment) {
+    this.setState(prevState => {
+      return {
+        comments: prevState.comments.concat(comment)
+      };
+    });
+  }
+
+  render() {
+    return (
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-half is-offset-one-quarter">
+              <CommentBox handleAddComment={this.handleAddComment} />
+              <Comments comments={this.state.comments.reverse()} />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default App;
