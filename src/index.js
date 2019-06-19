6c7dde0d188f29baf149297bdcce1d0945f5d998
Ron Gilmour <rgilmour@d25s80qcgg7d.campus.ithaca.lan>
2019-06-19 08:15:47 -0400

    installed sass and bootstrap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.scss';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
+import 'bootstrap/dist/css/bootstrap.min.css';
 require('./ably');
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
